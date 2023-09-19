46dc10742ec17c937cfb3426c75eb926d8039a68
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-09-19 13:59:09 -0300

    add transactions

--- queries.sql
+++ queries.sql
@@ -15,3 +15,35 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 
 SELECT * FROm animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+
+BEGIN; 
+UPDATE animals SET species = 'unspecified';
+SELECT * from animals;
+ROLLBACK;
+
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+
+BEGIN; 
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete_animals;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO delete_animals;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
