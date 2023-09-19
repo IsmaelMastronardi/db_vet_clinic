4863ad312c18d318a164890a8e8d8a42e10fcad3
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-09-19 14:29:23 -0300

    add queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- DAY 1
+
 SELECT * FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
@@ -15,6 +17,24 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 
 SELECT * FROm animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+--DAY 2
+
+SELECT COUNT (*) FROM animals;
+
+SELECT COUNT (*) FROM animals WHERE escape_attempts < 1;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, AVG(escape_attempts) AS escape_average FROM animals GROUP BY (neutered);
+
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31'
+GROUP BY species;
 
 
 
