09caf0dcd9593974b5a889a164604aba02dbb2be
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-09-20 01:19:39 -0300

    add owners and species tables to schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
+-- DAY 1
+
 CREATE TABLE animals (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
@@ -8,4 +10,18 @@ CREATE TABLE animals (
     neutered BOOLEAN NOT NULL,
     weight_kg DECIMAL(10,2) NOT NULL,
     species VARCHAR(100)
+);
+
+
+-- DAY 3
+
+CREATE TABLE owners (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+CREATE TABLE species (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50)
 );
